89cbef14f476f4bdf4fe2ed649ce48f4e8eafcfe
Yılmaz Edis <ylmazedis@gmail.com>
2023-04-07 22:22:05 +0300

    init project

---
+++ MovieStreamTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieStreamTime
+//
+//  Created by yilmaz on 7.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
